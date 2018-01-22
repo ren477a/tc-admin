09f725a2bac83e8c2edb39864cffeafaa45320cd
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-22 19:04:47 +0800

    Setup api calls

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -5,7 +5,40 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  base: String = 'http://travelcatalog.me/api/'
 
-  // CRUD
+  constructor(private http: Http) { 
+  }
+
+  create(user) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'users'
+    return this.http.post(ep, user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  read(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'users/' + id
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  readAll() {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'users'
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  delete(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'users/' + id
+    return this.http.delete(ep, {headers: headers})
+      .map(res => res.json());
+  }
 }
