5da0e291fe9bcc74080f4ae77f02f18b5544652c
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-22 19:04:33 +0800

    Setup api calls

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -5,7 +5,42 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TransactionsService {
 
-  constructor() { }
+  base: String = 'http://travelcatalog.me/api/'
 
+  constructor(private http: Http) { 
+  }
+
+  // CRUD
+  create(transaction) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'transactions'
+    return this.http.post(ep, transaction, {headers: headers})
+      .map(res => res.json());
+  }
+
+  read(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'transactions/' + id
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  readAll() {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'transactions'
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  delete(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'transactions/' + id
+    return this.http.delete(ep, {headers: headers})
+      .map(res => res.json());
+  }
   // CRUD
 }
