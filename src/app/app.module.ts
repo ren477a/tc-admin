157d96b8bf911533ca407db0e9e1b729b0f80dbf
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-22 18:30:47 +0800

    Import services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AuthService } from './services/auth.service';
+import { ToursService } from './services/tours.service';
+import { AgenciesService } from './services/agencies.service';
+import { CashoutsService } from './services/cashouts.service';
+import { TransactionsService } from './services/transactions.service';
+import { UsersService } from './services/users.service';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +18,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AgenciesService,
+    CashoutsService,
+    ToursService,
+    TransactionsService,
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
