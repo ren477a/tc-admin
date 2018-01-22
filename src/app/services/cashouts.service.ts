e737ead902499b65f5592e131c8076235c2cecd4
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-22 18:59:02 +0800

    Setup api calls

--- src/app/services/cashouts.service.ts
+++ src/app/services/cashouts.service.ts
@@ -5,8 +5,54 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CashoutsService {
 
-  constructor() { }
+  base: String = 'http://travelcatalog.me/api/'
+
+  constructor(private http: Http) { 
+  }
+
+
+  // Process cashout
+  process(id, status, msg) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'cashouts/' + id
+    return this.http.put(ep, {status: status, msg: msg}, {headers: headers})
+      .map(res => res.json());
+  }
 
   // CRUD
+  create(cashout) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'cashouts'
+    return this.http.post(ep, cashout, {headers: headers})
+      .map(res => res.json());
+  }
+
+  read(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'cashouts/' + id
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  readAll() {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'cashouts'
+    return this.http.get(ep, {headers: headers})
+      .map(res => res.json());
+  }
+
+  delete(id) {
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    let ep = this.base + 'cashouts/' + id
+    return this.http.delete(ep, {headers: headers})
+      .map(res => res.json());
+  }
+  // CRUD
   // Process cashout
+  
 }
